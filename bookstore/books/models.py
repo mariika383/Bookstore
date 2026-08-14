578d3d49a6e3f3b0d5b07b02fdae99e2e5e1dc4d
Mariia Lazareva <mariika383@gmail.com>
2026-08-14 16:13:58 +0300

    Models

---
+++ bookstore/books/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    stock = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
